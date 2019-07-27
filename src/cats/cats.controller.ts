0d0b570eec7644ca57c1c4ec59b521fa02cd54ab
Mirla Chucre <mirlabraga@gmail.com>
2019-07-27 15:27:33 +1000

    created interface, add post method

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { CatsService } from 'src/cats/cats.service';
+import { Cat } from 'src/cats/cat';
 
 @Controller('cats')
 export class CatsController {
@@ -9,5 +10,10 @@ export class CatsController {
   @Get()
   async findAll(): Promise<any> {
     return this.catsService.findAll();
+  }
+
+  @Post()
+  async create(cat: Cat) {
+    return this.catsService.create(cat);
   }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,9 +1,17 @@
 import { Injectable, Get } from '@nestjs/common';
+import { Cat } from 'src/cats/cat';
 
 @Injectable()
 export class CatsService {
 
+  private readonly cats: Cat[] = [];
+
   findAll(): any {
-    return "cats";
+    return this.cats;
   }
+
+  create(cat: Cat) {
+    this.cats.push(cat);
+  }
+
 }
