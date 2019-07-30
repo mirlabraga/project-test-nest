fdad4d57532377e0a637afb73c146c13aec4c8d3
Mirla Chucre <mirlabraga@gmail.com>
2019-07-30 22:19:44 +1000

    Add configurate of the MongoDB

--- README.md
+++ README.md
@@ -8,6 +8,17 @@ For backend I think NojeJS is a good option for fast and small project.
 More about Nest, please link there:
 [Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
 
+## Details
+
+There are two entities for endpoit. First, the Cats, this is an samples's default of the Nest's documentation. And, Employees, it is saving in the MongoDB.
+
+## Development Requirements
+
+- node >= v8 [![node](https://img.shields.io/badge/node-v8-blue.svg?cacheSeconds=2592000)](https://nodejs.org/en/download/)
+- npm
+- mongodb >= v4
+- Sample object's json: http://dummy.restapiexample.com/api/v1/employees
+
 ## Installation
 
 ```bash
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatSchema } from 'schemas/cat.schema';
+import { ConfigModule } from 'src/config/config.module';
+import { EmployeesController } from './employees/employees.controller';
+import { EmployeesService } from './employees/employees.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule, ConfigModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/test'),
+    MongooseModule.forFeature([{ name: 'Cat', schema: CatSchema }]),
+    MongooseModule.forFeature([{ name: 'Employees', schema: CatSchema }])],
+  controllers: [AppController, CatsController, EmployeesController],
+  providers: [AppService, CatsService, EmployeesService],
 })
-export class AppModule {}
+export class AppModule { }
